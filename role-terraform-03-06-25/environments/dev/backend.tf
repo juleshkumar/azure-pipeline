68c91a8ee6daa501b1676ff805538b2efbe43818
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2025-06-19 22:40:35 +0530

    Update backend.tf

--- role-terraform-03-06-25/environments/dev/backend.tf
+++ role-terraform-03-06-25/environments/dev/backend.tf
@@ -5,10 +5,10 @@ terraform {
     region     = "us-east-1" 
     #use_lockfile = true  #S3 native locking
     
-    assume_role = {
-    role_arn = "arn:aws:iam::296062546708:role/terraform-deployment-test-role"
-    session_name = "terraform-session"
-  }
+    #assume_role = {
+    #role_arn = "arn:aws:iam::296062546708:role/terraform-deployment-test-role"
+    #session_name = "terraform-session"
+  #}
   }
 }
 
